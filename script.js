a49a9d87e143f55c2096a12ab981fe75be43ddb0
Pouriyatarigiyan <Pouriyatarigiyan@gmail.com>
2025-05-25 14:02:23 +0330

    آپدیت جدید

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-background-image: url('images/bg.jpg');
+// اجرای انیمیشن موشن هنگام بارگذاری
+window.addEventListener("load", () => {
+  document.querySelectorAll(".section").forEach(section => {
+    section.classList.add("visible");
+  });
+});
